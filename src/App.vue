6ae20bb50914cca57c6ef0f248868c0287a59273
Jisub Kim <plumssi9@gmail.com>
2020-04-28 00:14:19 +0900

    implement skeleton of upload

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,23 @@
 <template>
-  <div id="app">
-
+  <v-app id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-      
+      <router-link to="/">Home</router-link>|
+      <router-link to="/about">About</router-link>|
+      <router-link to="/test">Test</router-link>
     </div>
-    
-    <router-view/>
-        <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <HelloWorld test="1+1"/>
 
-  </div>
+    <router-view />
+    <!-- <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld test="1+1" /> -->
+  </v-app>
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans&display=swap');
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  /* font-family: Avenir, Helvetica, Arial, sans-serif; */
+  font-family: 'Open Sans', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/views/Test.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="card-area">
+    <template v-for="(item, i) in items">
+      <v-card :key="i" width="600" color="#ffffff" class="item">
+        <v-card-title class="card-title">
+          <div class="text">{{item.title}}</div>
+          <v-btn small fab tile text color="gray">
+            <v-icon>mdi-pencil</v-icon>
+          </v-btn>
+          <v-spacer></v-spacer>
+          <v-btn icon v-on="on">
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </v-card-title>
+        <v-divider />
+        <v-card-subtitle>
+          <div class="sub-title">Add Image Samples:</div>
+          <div class="button-area">
+            <!-- <v-btn width="80" height="60" color="#E3F2FD" large depressed class="icon-btn">
+              <v-icon color="#1565C0">videocam</v-icon>
+              <div class="text">Webcam</div>
+            </v-btn> -->
+            <v-btn width="80" height="60" color="#E3F2FD" large depressed class="icon-btn">
+              <v-icon color="#1565C0">mdi-cloud-upload</v-icon>
+              <div class="text">Upload</div>
+            </v-btn>
+          </div>
+        </v-card-subtitle>
+      </v-card>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    items: [
+      {
+        title: "Class 1"
+      },
+      {
+        title: "Class 2"
+      }
+    ]
+  })
+};
+</script>
+
+<style lang="scss">
+.card-area {
+  .v-card.item {
+    border-radius: 12px;
+    margin: 20px;
+  }
+  .card-title {
+    padding: 8px 16px;
+    font-size: 20px;
+    font-weight: 500;
+  }
+}
+.sub-title {
+  font-size: 16px;
+  font-weight: 500;
+}
+.button-area {
+  margin-top: 15px;
+  .v-btn__content {
+    flex-direction: column;
+  }
+  .icon-btn {
+    margin: 0 3px;
+    &:first-of-type {
+      margin-left: 0px;
+    }
+    &:last-of-type {
+      margin-right: 0px;
+    }
+  }
+  .text {
+    color: #1565c0;
+    font-size: 12px;
+    text-transform: capitalize;
+    margin-top: 5px;
+  }
+}
+</style>
