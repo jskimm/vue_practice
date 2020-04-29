97414f8705cc65632f7d3fbd2f8e634b41d98021
Jisub Kim <plumssi9@gmail.com>
2020-04-29 18:09:59 +0900

    vue-input-autowidth

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import VueInputAutowidth from 'vue-input-autowidth'
+Vue.use(VueInputAutowidth)
 
 Vue.config.productionTip = false
 
