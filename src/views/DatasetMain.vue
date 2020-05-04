bc222d5e916469f4d96944c1cc5a5f73ae4ad02f
Jisub Kim <plumssi9@gmail.com>
2020-05-05 00:29:32 +0900

    implement grid card components to DatasetMain

--- src/views/DatasetMain.vue
+++ src/views/DatasetMain.vue
@@ -1,144 +1,114 @@
 <template>
   <v-container>
-    <div class="card-area" style="display:inline-block">
-      <!-- <v-card>
-            <v-card-title>
-                test
-            </v-card-title>
-        </v-card>
-      <template v-for="(item, i) in this.items">
-        <v-card :key="i" width="650" color="#ffffff" class="item">
-          <v-card-title class="card-title">
-            <v-btn small fab text color="gray" @click="fixTitle(item)">
-              <v-icon v-show="!item.nowModify" color="#BDC1C6">mdi-lock-open-outline</v-icon>
-              <v-icon v-show="item.nowModify" color="#BDC1C6">mdi-lock-outline</v-icon>
+    <grid
+      :draggable="draggable"
+      :sortable="true"
+      :items="datasets"
+      :cellWidth="212"
+      :cellHeight="272"
+      :gridWidth="1100"
+    >
+      <template slot="cell" scope="props">
+        <template v-if="props.item.type === 'add'">
+          <v-row
+            justify="center"
+            align-content="center"
+            id="add-button"
+            class="add-button"
+            style="height: 100%; padding: 12px;"
+            @mousedown="handleMousedown(props.item)"
+          >
+            <v-btn fab color="#42b983" @click="addDataset">
+              <v-icon color="white">mdi-plus</v-icon>
             </v-btn>
-            <v-spacer></v-spacer>
-            <v-btn icon>
-              <v-icon>mdi-dots-vertical</v-icon>
-            </v-btn>
-          </v-card-title>
-      </v-card>-->
-      <v-container>
-        <v-row dense>
-          <v-col cols="6">
-            <v-card color="#385F73" dark max-width="650"
-      class="mx-auto">
-              <v-card-title class="headline">Unlimited music now</v-card-title>
+          </v-row>
+        </template>
+        <template v-else>
+          <gridcard @mousefunction="handleMousedown" :item="props.item"></gridcard>
+        </template>
+      </template>
+    </grid>
 
-              <v-card-subtitle>Listen to your favorite artists </v-card-subtitle>
-
-              <v-card-actions>
-                <v-btn text>Listen Now</v-btn>
-              </v-card-actions>
+    <!-- <div class="card-area" style="display:inline-block">
+        <v-row style="min-width:800px">
+          <v-col v-for="(item, i) in datasets" :key="i" cols="3">
+            <v-card class="mx-auto" max-width="200" height="260px">
+              <v-img :src="item.src" height="150px" />
+              <v-card-title class="headline">  
+              <v-card-title>{{item.title}}</v-card-title>
+              <v-card-subtitle class="text-left">{{item.subtitle}}</v-card-subtitle>
             </v-card>
           </v-col>
-
-          <v-col v-for="(item, i) in items" :key="i" cols="6">
-            <v-card :color="item.color" dark max-width="650"
-      class="mx-auto">
-              <div class="d-flex flex-no-wrap justify-space-between">
-                <div>
-                  <v-card-title class="headline" v-text="item.title"></v-card-title>
-
-                  <v-card-subtitle v-text="item.artist"></v-card-subtitle>
-                </div>
-
-                <v-avatar class="ma-3" size="125" tile>
-                  <v-img :src="item.src"></v-img>
-                </v-avatar>
-              </div>
-            </v-card>
+          <v-col cols="3" align-self="center" style="height:260px;padding:12px">
+                <v-btn fab color="#42b983" @click="addDataset">
+                  <v-icon color="white">mdi-plus</v-icon>
+                </v-btn>
           </v-col>
         </v-row>
-      </v-container>
-    </div>
+    </div>-->
   </v-container>
 </template>
 
 <script>
+import GridCard from "../components/GridCard.vue";
 export default {
+  components: {
+    gridcard: GridCard
+  },
   data() {
     return {
-      items: [
+      draggable: false,
+      datasets: [
         {
-          color: "#1F7087",
-          src: "https://cdn.vuetifyjs.com/images/cards/foster.jpg",
-          title: "Supermodel",
-          artist: "Foster the People"
+          type: "add",
         },
         {
-          color: "#952175",
-          src: "https://cdn.vuetifyjs.com/images/cards/halcyon.png",
-          title: "Halcyon Days",
-          artist: "Ellie Goulding"
-        }
-      ],
-
-      items2: [
-        {
-          title: "Class 1",
-          nowModify: false
+          src: "https://miro.medium.com/max/245/1*nlfLUgHUEj5vW7WVJpxY-g.png",
+          title: "MNIST",
+          subtitle: "handwritten digits"
         },
         {
-          title: "Class 2",
-          nowModify: false
+          src:
+            "https://storage.googleapis.com/kaggle-competitions/kaggle/3362/media/woof_meow.jpg",
+          title: "Cats vs Dogs",
+          subtitle: "Ellie Goulding"
         }
       ]
     };
   },
-  methods: {}
+  methods: {
+    addDataset: function() {
+      this.datasets.push({
+        src: `https://storage.googleapis.com/kaggle-competitions/kaggle/3362/media/woof_meow.jpg`,
+        title: "Test",
+        subtitle: "subtitle Test"
+      });
+      // #TODO: 서버에서 데이터 가져오기
+    },
+    handleMousedown: function(item) {
+      if(item.type === "add") {
+        this.draggable = false;
+      }
+      else {
+        this.draggable = true;
+      }
+    }
+  }
 };
 </script>
 
 <style lang="scss">
-.v-text-field {
-  width: 4000px;
+.add-button {
+  height: 100%;
+  padding: 12px;
 }
-
-.card-area {
-  .v-card.item {
-    border-radius: 12px;
-    margin: 20px;
-  }
-  .card-title {
-    padding: 8px 16px;
-    font-size: 20px;
-    font-weight: 500;
-  }
-}
-.sub-title {
-  font-size: 16px;
-  font-weight: 500;
-}
-.button-area {
-  margin-top: 15px;
-  .v-btn__content {
-    flex-direction: column;
-  }
-  .icon-btn {
-    margin: 0 3px;
-    &:first-of-type {
-      margin-left: 0px;
-    }
-    &:last-of-type {
-      margin-right: 0px;
-    }
-  }
-  .text {
-    color: #1565c0;
-    font-size: 12px;
-    text-transform: capitalize;
-    margin-top: 5px;
-  }
-}
-.add_classes {
+.add_dataset {
   position: relative;
   cursor: pointer;
   width: 100%;
   outline: 0;
   border: 1.5px dashed #bdc1c6;
-  border-radius: 12px;
+  border-radius: 4px;
   background: transparent;
   overflow: hidden;
   text-transform: none !important;
